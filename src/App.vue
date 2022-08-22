eef2550cc616f1c4d6ce7ee94812da8d07086e67
Thaw Zin Soe <devuser@7thcomputing.com>
2022-08-22 10:52:56 +0630

    UI

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1 id="title">How Fast Can You Catch Me ? </h1>
+  <button id="btn" @click="start">Play</button>
+</template>
+
+<script>
+export default{
+  name : "App",
+  components:{
+    
+  },
+  data(){
+    return{
+          isPlaying : false
+    }
+  },
+  methods:{
+    start(){
+      this.isPlaying = true;
+    }
+  }
+}
+</script>
+
+<style>
+  #title{
+    text-align: center;
+    padding-top: 20px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+  #btn{
+    margin-left: 45% !important;
+    display: inline-block;
+    background-color: black;
+    color: white;
+    padding: 10px 40px;
+    border-radius: 5px;
+  }
+  #btn:hover{
+    background-color:gray ;
+  }
+</style>
